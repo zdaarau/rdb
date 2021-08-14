c865c06c5d2bf62c464d3f6cbf910b5bde3890e5
Salim B <git@salim.space>
2021-08-15 01:26:45 +0200

    define rd families

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -1,4 +1,6 @@
 list(
-  rd_family_title = list(metadata = "Other metadata functions:",
+  rd_family_title = list(api_status = "Other C2D API status functions",
+                         metadata = "Other referendum metadata functions:",
+                         referendum = "Other referendum data functions:",
                          tags = "Other tag-related functions:")
 )
