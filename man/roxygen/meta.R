3ed0d06bedf82c5267aa5c8414c767f59c0b1784
Salim B <git@salim.space>
2021-11-07 20:17:25 +0100

    cosmetic changes

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -1,5 +1,5 @@
 list(
-  rd_family_title = list(api_status = "Other C2D API status functions",
+  rd_family_title = list(api_status = "Other C2D API status functions:",
                          metadata = "Other referendum metadata functions:",
                          referendum = "Other referendum data functions:",
                          sudd = "Other sudd.ch scraping functions:",
